52263d0144770b998cbecbdd0b2c35a04204fc93
Mostafa Rashad <144150314+Mostafa-Rashad0@users.noreply.github.com>
2026-07-04 18:43:38 +0300

    Stabilize ShellCheck CI

--- lib/logger.sh
+++ lib/logger.sh
@@ -5,18 +5,27 @@ HI_COLOR_INFO='\033[34m'
 HI_COLOR_SUCCESS='\033[32m'
 HI_COLOR_WARNING='\033[33m'
 HI_COLOR_ERROR='\033[31m'
+HI_LOG_ENABLED=0
 HI_COLOR_RESET='\033[0m'
 
 hi_init_logger() {
   if [ "$(id -u)" -eq 0 ]; then
     touch "$HI_LOG_FILE"
     chmod 0644 "$HI_LOG_FILE"
+    HI_LOG_ENABLED=1
+  else
+    HI_LOG_ENABLED=0
   fi
 }
 
 hi_log() {
   level=$1
   message=$2
+
+  if [ "$HI_LOG_ENABLED" -ne 1 ]; then
+    return 0
+  fi
+
   timestamp=$(date '+%Y-%m-%dT%H:%M:%S%z')
   printf '%s %s %s\n' "$timestamp" "$level" "$message" >> "$HI_LOG_FILE"
 }
